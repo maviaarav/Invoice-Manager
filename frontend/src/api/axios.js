a45ca449ac9aaa21224a4d3839381b1ca5e944b1
Aarav <aarav@192.168.1.9>
2026-07-12 17:33:29 +0530

    reverted

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-const instance = axios.create({
-    baseURL: '/backend',
-    withCredentials: true
-})
 
-export default instance
+const instance = axios.create({
+    baseURL: "https://maviaarav-invoice-api-d7aqcgd7gaagh7eg.centralindia-01.azurewebsites.net",
+    withCredentials: true
+});
+
+export default instance;
